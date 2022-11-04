9f88135895d8b345f5efe3f1f7a96edd53a1e360
Fuat17h <fuat17h@gmail.com>
2022-11-04 17:00:00 +0300

    second commit

--- CmsShoppingCart/Models/Page.cs
+++ CmsShoppingCart/Models/Page.cs
@@ -5,11 +5,11 @@ namespace CmsShoppingCart.Models
     public class Page
     {
         public int Id { get; set; }
-        [Required]
+        [Required, MinLength(2, ErrorMessage = "Minimum length is 2")]
         public string Title { get; set; }
-        [Required]
+        //[Required]
         public string Slug { get; set; }
-        [Required]
+        [Required, MinLength(4, ErrorMessage = "Minimum length is 4")]
         public string Content { get; set; }
         public int Sorting { get; set; } 
     }
